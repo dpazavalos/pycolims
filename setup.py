a2a1395088908aca05dabbad5a71a02789a3ae9e
Daniel Avalos <dpazavalos@protonmail.com>
2019-01-27 00:33:03 -0600

    Add "Select All" to SelectMulti Refactor screen clearing to use native command Add file documentation Behind the scenes work, pylint conformity

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setuptools.setup(
     name="pycolims",
-    version="0.1",
+    version="0.1.1",
     author="Daniel Paz Avalos",
     author_email="dpazavalos@protonmail.com",
     description="An importable, single stage CLI menu package",
