8b1a21992a0de5c8e9125a9155f9415503b599ac
Daniel Avalos <dpazavalos@protonmail.com>
2019-02-04 14:31:53 -0600

    Adjust single menu obj to match Tools refactoring. Refactor page and list prep into separate functions

--- pycolims/single.py
+++ pycolims/single.py
@@ -4,17 +4,21 @@ from typing import List, Tuple
 
 class SelectSingle(_Menu):
     """Given a list, prompt for selection of a single item\n
-    Returns the selected item\n
-    List can be flat or an array\n\n
-    Once init, call run()"""
+    Returns the selected item\n"""
 
     def prep_page(self):
         """Use self.page.reset to prepare page turners"""
-        self.page.reset(only_turners=[' ', ' '],
+        '''self.page.reset(only_turners=[' ', ' '],
                         frst_turners=[' ', '+'],
                         midl_turners=['-', '+'],
                         last_turners=['-', ' '],
-                        page_options=['<>'])
+                        page_options=['<>'])'''
+        self.page.reset(only_turners=[],
+                        frst_turners=['+'],
+                        midl_turners=['-', '+'],
+                        last_turners=['-', ],
+                        page_options=['<>'],
+                        goto_multipliers=self.generate_goto_multipliers())
 
     def prep_given_list(self):
         """Modify self.menu_list, inserting index [orig_ndx, *desired_item(s)]"""
@@ -30,36 +34,32 @@ class SelectSingle(_Menu):
         """Select Single Navigator
         Sizes list according to term height, navigates pages, returns a single selection"""
 
-        self.prep_page()
         self.prep_given_list()
+        self.prep_page()
 
-        goto_multipliers: List[int] = self.generate_goto_multipliers()
-        nav_options: List[List[List[str]]] = self.generate_nav_options(goto_multipliers)
-
-        # Used to determine which section of menu_list to send
-        goto_multi = 0
+        # goto_multipliers: List[int] = self.generate_goto_multipliers()
+        # nav_options: List[List[List[str]]] = self.generate_nav_options(goto_multipliers)
+        # goto_multi = 0
         """Multiply by self.term_height to determine starting index"""
 
         self.statics.clear_screen()
-        while True:
-            disp_start: int = goto_multi * self.term.height
 
-            command = self.displayer(self.given_list[disp_start: (disp_start + self.term.height)],
-                                     nav_options[goto_multi])
+        selected_val = ''
+        while selected_val == '':
+            disp_start: int = self.page.goto_multi * self.term.height
+
+            command = self.displayer(self.given_list[disp_start:disp_start+self.term.height],
+                                     self.page.active_turners)
             """Command is the returned str from displayer (nav option or a single menu selection)"""
 
-            if not self.valid_navigator(command):
-                selected_val = self.given_list[int(command)][1::]
-                """Selection pulled from menu_list, based on command. Value to be returned"""
-                break
+            if command in ['+', '-']:
+                self.page.mentum(command)
 
-            else:  # Try navigating between chunks (by '+' or '-' characters)
-                try:
-                    if self.statics.mentum(goto_multi, command) in goto_multipliers:
-                        goto_multi = self.statics.mentum(goto_multi, command)
-                except (KeyError, IndexError):
-                    # Key and Index errors cover the possible "Next page when last page"
-                    pass
+            elif self.valid_option(command):
+                # Call command handler
+                pass
+            else:
+                selected_val = self.given_list[int(command)][1::]
 
         if len(selected_val) == 1:
             return selected_val[0]
