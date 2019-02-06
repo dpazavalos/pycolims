1f4eb4757010a83834990979d610580a19907770
Daniel Avalos <dpazavalos@protonmail.com>
2019-02-06 10:52:51 -0600

    Update req to 3.7 (for dataclass usage)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dpazavalos/pycolims",
     packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3.5",
+    classifiers=["Programming Language :: Python :: 3.7",
                  "License :: OSI Approved :: MIT License",
                  "Operating System :: OS Independent"],
 )
