23d7777243bff052be5e5b3a93bf5574fbcc0818
Daniel Avalos <dpazavalos@protonmail.com>
2019-02-05 20:50:52 -0600

    Add easy import pointers to init

--- pycolims/__init__.py
+++ pycolims/__init__.py
@@ -1,1 +1,19 @@
 name = 'pycolims'
+
+
+class Single:
+    from pycolims.menus.Menu_Factory import build_single, SingleMenu
+    run: SingleMenu.run
+
+    def __init__(self):
+        single = self.build_single.new_single_menu_obj()
+        self.run = single.run
+
+
+class Multi:
+    from pycolims.menus.Menu_Factory import MultiMenu, build_multi
+    run: MultiMenu.run
+
+    def __init__(self, ):
+        multi = self.build_multi.new_multi_menu_obj()
+        self.run = multi.run
